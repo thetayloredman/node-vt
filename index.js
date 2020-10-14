d852915a6836e160c813a5d73e764287a88d14a8
thetayloredman <nutdriver716@gmail.com>
2020-10-14 11:31:17 -0700

    Revert "export"
    This reverts commit a1d12910b114102897323dc7da0c6a49d2debce8.

--- index.js
+++ index.js
@@ -16,5 +16,108 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-// Export
-export * from './classes/index.js';
+// Modules
+const { request } = require('https');
+
+// Pre-defined vars
+const apiSettings = {
+    host: 'www.virustotal.com',
+    apiRoot: '/api/v3'
+}
+
+// Validation
+const validMethods = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH'
+]
+
+// Classes
+class Err extends Error {
+    /**
+     * Creates a new custom error (With message)
+     * @constructor
+     * @param {String} message The error message
+     * @param {String} [type] The type of error (Like "TypeError")
+     * @returns {undefined} undefined
+     * @example
+     * throw new Err('Example', 'ExampleError'); // => Uncaught Err [Error]: [NodeVTError] ExampleError: Example
+     */
+    constructor(message, type) {
+        super(`${type ? type + ': ' : ''}${message}`);
+        this.name = '[NodeVTError]'
+    }
+}
+
+class URLParams {
+    constructor(data) {
+        this.data = data;
+        this.string = null;
+        this._validateType(data);
+        this._init();
+    }
+    _validateType(data) {
+        let s = this._checkType(data);
+        if (s === null) {
+            // no data
+            throw new Err('URLParams takes 1 argument: provided 0', 'ArgumentCountError');
+        } else if (s === false) {
+            // bad type
+            throw new Err('URLParams was passed an invalid type: expected Object', 'TypeError');
+        } else {
+            // ok, we return
+            return;
+        }
+    }
+    _checkType(data) {
+        if (!data) {
+            return null;
+        } else if (typeof data !== 'object') {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    _init() {
+        let d = this.data;
+        d = Object.entries(d);
+        if (d.length === 0) {
+            this.string = '';
+        } else {
+            for (let i of d) {
+                this._appendString(i[0], i[1]);
+            }
+        }
+    }
+    _appendString(key, val) {
+        if (this.string) {
+            this.string += `&${this._urlEncode(key)}=${this._urlEncode(val)}`
+        } else {
+            this.string = `${key}=${val}`
+        }
+    }
+    _urlEncode(data) {
+        let out = data;
+
+        // Parse data types
+        let t = typeof out;
+        switch (t) {
+            case 'string':
+                break;
+            case 'boolean':
+                out = out.toString();
+                break;
+            case 'object':
+                out = JSON.stringify(out);
+                break;
+            default:
+                break;
+        }
+
+        // Parse URLEncoded stuff
+        out = encodeURIComponent(out);
+
+        return out;
+    }
+}
