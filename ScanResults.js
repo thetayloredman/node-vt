4b20f973d6185976a70b6bd34f157f156c407d5c
thetayloredman <nutdriver716@gmail.com>
2020-10-15 13:25:07 -0700

    Update ScanResults.js

--- ScanResults.js
+++ ScanResults.js
@@ -23,19 +23,47 @@ const { Err, Response } = require('./index.js');
 
 // Main
 /**
- * Represents scan results from the API
+ * Represents scan results from the API.
+ * @property {Object} results The raw scan results
+ * @property {Object} stats The last analysis stats
+ * @property {Number} stats.harmless The number of engines that say OK.
+ * @property {Number} stats.malicious The number of engines that flag the file.
+ * @property {Number} stats.suspicious The number of engines that are suspicious about the file.
+ * @property {Number} stats.timeout The number of engined that timed out.
+ * @property {Number} stats.unsupported The number of engines that don't support that type.
+ * @property {Number} stats.undetected The engines that say this file is OK.
  */
 class ScanResults {
     /**
      * Creates scan results
      * @constructor
-     * @param {Response} data The response from the API. Warning: The Response MUST have been resolved!
+     * @param {Response} data The response from the API when performing GET /files/{id}. Warning: The Response MUST have been resolved!
      * @example
      * const results = new ScanResults(res.data.json); // => ScanResults
      */
     constructor(data) {
         if (!data instanceof Response) {
-            throw new Err('ScanResults takes a Response, not a ' + typeof data)
+            throw new Err('ScanResults takes a Response, not a ' + typeof data, 'TypeError');
+        } else {
+            if (!data.isJson) {
+                throw new Err('Failed to get a valid API Response. Is it a Response that is RESOLVED?', 'InvalidResponseError');
+            } else {
+                const { data: fileData } = data.data.json;
+                this.stats = fileData.data.last_analysis_stats;
+                const results = fileData.data.attributes.last_analysis_results;
+
+                // Process aliases
+                const pResults = new Object();
+                for (let [ k, v ] of Object.entries(results)) {
+                    if (!k === 'type-unsupported') {
+                        pResults[k] = v;
+                    } else {
+                        pResults.unsupported = v;
+                    }
+                }
+
+                this.stats = pResults;
+            }
         }
     }
 }
